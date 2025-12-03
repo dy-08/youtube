c020cbf1b18ca651c80283dbc755fb038405d4d8
Youngho Kwon <dy-08@naver.com>
2025-12-03 13:55:14 +0900

    feat: 검색기능(키워드 활용)

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,23 @@ import Short from './pages/Short';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <NotFound />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: 'short', element: <Short /> },
-      { path: 'videos', element: <Videos /> },
-      { path: 'videos/:videoId', element: <VideoDetail /> },
-    ],
-  },
+    {
+        path: '/',
+        element: <Root />,
+        errorElement: <NotFound />,
+        children: [
+            { index: true, element: <Home /> },
+            { path: 'short', element: <Short /> },
+            { path: 'videos/:keyword', element: <Videos /> },
+        ],
+    },
 ]);
 
 const queryClient = new QueryClient();
 export default function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
+    );
 }
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,5 +1,37 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+import Card from '../components/Card';
 
 export default function Videos() {
-    return <div>Videos</div>;
+    const { keyword } = useParams();
+    const {
+        isLoading,
+        error,
+        data: contents,
+    } = useQuery({
+        queryKey: ['keyword'],
+        queryFn: async () => {
+            return fetch(
+                `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${keyword}&key=AIzaSyC8t3lJJURohhFXjaA4dDmwfjPivbchDR8`
+            ).then((res) => res.json());
+        },
+    });
+    if (isLoading) return <p>Loading...</p>;
+    if (error) return <p>{error}</p>;
+    const { items } = contents;
+    console.log(items);
+    return (
+        <ul className='m-auto grid grid-cols-3 gap-2 place-items-center place-content-center'>
+            {items.map((item) => (
+                <li key={item.id.videoId} className='w-full p-2'>
+                    <Card
+                        thumbnail={item.snippet.thumbnails}
+                        title={item.snippet.title}
+                        channelTitle={item.snippet.channelTitle}
+                        publishedAt={item.snippet.publishedAt}
+                    />
+                </li>
+            ))}
+        </ul>
+    );
 }
